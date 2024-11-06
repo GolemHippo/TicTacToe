50dc4312c04699da3847ddb78bb438e3043a4e89
Hippo <hippolyte.geslain@laplateforme.io>
2024-11-06 12:52:18 +0100

    On avance très fort

--- MatriceHippo.py
+++ MatriceHippo.py
@@ -6,13 +6,44 @@ def plateau_jeu():
     print("", board[3],"|", board[4],"|", board[5],"")
     print("---|---|---")
     print("", board[6],"|", board[7],"|", board[8],"")
+    
+def movedujoueur():
+    case_jouée=int(input('Où voulez-vous jouez?'))
+    if board[case_jouée-1]==' ':
+        board[case_jouée-1]=Joueur_actuel
+    else:
+        print('Vous devez choisir une bonne case!!!')
+
 
 Joueur_actuel='X'
 
-def àqui():
+def changedejoueur():
     global Joueur_actuel    
     Joueur_actuel = 'O' if Joueur_actuel == 'X' else 'X'
 
+def cdts_vic_j1():
+    if all(cell=='X' for cell in board[0:3]):
+        print('GG WP J1')
+    elif all(cell=='X' for cell in board[3:6]):
+        print('GG WP J1')
+    elif all(cell=='X' for cell in board[6:9]):
+        print('GG WP J1')
+    elif board[0]==board[4]==board[8]!=' ':
+        print('GG WP J1')
+    elif board[2]==board[4]==board[6]!=' ':
+        print('GG WP J1')
 
-if all(cell=='X' for cell in  board):
+def cdts_vic_j2():
+    if all(cell=='O' for cell in board[0:3]):
+        print('GG WP J2')
+    elif all(cell=='O' for cell in board[3:6]):
+        print('GG WP J2')
+    elif all(cell=='O' for cell in board[6:9]):
+        print('GG WP J2')
+    elif 
+
+
+
+if all(cell=='X' for cell in board):
     print('Player X wins')
+
