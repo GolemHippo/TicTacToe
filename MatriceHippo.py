4ea2f2afbb60f4e34f3327135663ca514eb6395d
Val_laplat <valentin.rossi@laplateforme.io>
2024-11-07 11:32:05 +0100

    test pas sur

--- MatriceHippo.py
+++ MatriceHippo.py
@@ -1,9 +1,109 @@
-Grid1=[1,2,3]
-Grid2=[4,5,6]
-Grid3=[7,8,9]
+board=[" " for _ in range(9)]
 
-print("",Grid1[0],"|",Grid1[1],"|",Grid1[2],"")
-print("---|---|---")
-print("",Grid2[0],"|",Grid2[1],"|",Grid2[2],"")
-print("---|---|---")
-print("",Grid3[0],"|",Grid3[1],"|",Grid3[2],"")
+Joueur_actuel='X'
+
+result='ingame'
+
+def plateau_jeu():
+    print("", board[0],"|", board[1],"|", board[2],"")
+    print("---|---|---")
+    print("", board[3],"|", board[4],"|", board[5],"")
+    print("---|---|---")
+    print("", board[6],"|", board[7],"|", board[8],"")
+
+
+
+def changedejoueur():
+    global Joueur_actuel    
+    Joueur_actuel = 'O' if Joueur_actuel == 'X' else 'X'
+
+def movedujoueur():
+    case_jouée=int(input('Où voulez-vous jouez?'))
+    if 1<=case_jouée<=9:
+        if board[case_jouée-1]==' ':
+            board[case_jouée-1]=Joueur_actuel
+            changedejoueur()
+        else:
+            print('Vous devez choisir une bonne case!!!')
+    else:
+        print(' Vous devez choisir une case comprise entre 1 et 9 !!!')
+
+
+
+def cdts_vic_j1():
+    global result
+    if all(cell=='X' for cell in board[0:3]):
+        result=1
+        print('GG WP J1')
+    elif all(cell=='X' for cell in board[3:6]):
+        result=1
+        print('GG WP J1')
+    elif all(cell=='X' for cell in board[6:9]):
+        result=1
+        print('GG WP J1')
+    elif board[0]==board[4]==board[8]=='X':
+        result=1
+        print('GG WP J1')
+    elif board[2]==board[4]==board[6]=='X':
+        result=1
+        print('GG WP J1')
+    elif board[0]==board[3]==board[6]=='X':
+        result=1
+        print('GG WP J1')
+    elif board[1]==board[4]==board[7]=='X':
+        result=1
+        print('GG WP J1')
+    elif board[2]==board[5]==board[8]=='X':
+        result=1
+        print('GG WP J1')
+def cdts_vic_j2():
+    global result
+    if all(cell=='O' for cell in board[0:3]):
+        result=2
+        print('GG WP J2')
+    elif all(cell=='O' for cell in board[3:6]):
+        result=2
+        print('GG WP J2')
+    elif all(cell=='O' for cell in board[6:9]):
+        result=2
+        print('GG WP J2')
+    elif board[0]==board[4]==board[8]=='O':
+        result=2
+        print('GG WP J2')
+    elif board[2]==board[4]==board[6]=='O':
+        result=2
+        print('GG WP J2')
+    elif board[0]==board[3]==board[6]=='O':
+        result=2
+        print('GG WP J2')
+    elif board[1]==board[4]==board[7]=='O':
+        result=2
+        print('GG WP J2')
+    elif board[2]==board[5]==board[8]=='O':
+        result=2
+        print('GG WP J2')
+    
+
+def draw():
+    global result
+    if all(cell!=' ' for cell in board):
+        result=0
+        print('Belle partie,\nMatch nul')
+
+while True:
+    if result=='ingame':    
+        plateau_jeu()
+        movedujoueur()
+        cdts_vic_j1()
+        cdts_vic_j2()
+        draw()
+    elif result=='1':
+        plateau_jeu()
+        break
+    elif result=='2':
+        plateau_jeu()
+        break
+    elif result=='0':
+        plateau_jeu()
+        break
+exit
