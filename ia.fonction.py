f229da3b0360c083eeaf53c2ce12efc1337b9760
emile <emile.lenain@laplateforme.io>
2024-11-08 15:26:34 +0100

    fonction IA random

--- ia.fonction.py
+++ ia.fonction.py
@@ -1,3 +1,13 @@
+import random 
+
 def ia(board,signe):
-    case_vide =  [i for i in range(len(board)) 
-                  if board[i] == " "]
+    cases_vide =  [i for i in range(len(board)) 
+                   if board[i] is board[i] == " "] 
+    if cases_vide:
+            choix = random(cases_vide)
+            board[choix] = signe
+            if signe:
+                print("O")
+            else:
+                print("il n'y a plus de case a jouer")
+
