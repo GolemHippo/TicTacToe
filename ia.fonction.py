bcfaf72bc72e162085e26fd54f07cc61a7fcc817
emile <emile.lenain@laplateforme.io>
2024-11-08 16:36:17 +0100

    version final de l'ia

--- ia.fonction.py
+++ ia.fonction.py
@@ -1,13 +1,8 @@
 import random 
-
+signe = "O"
 def ia(board,signe):
     cases_vide =  [i for i in range(len(board)) 
                    if board[i] is board[i] == " "] 
-    if cases_vide:
-            choix = random(cases_vide)
-            board[choix] = signe
-            if signe:
-                print("O")
-            else:
-                print("il n'y a plus de case a jouer")
+    choix = random(cases_vide)
+    board[choix] = signe
 
