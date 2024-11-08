4f25d2d9f3dd9d9e6a2c2485d9c425ac04d7ebd1
Val_laplat <valentin.rossi@laplateforme.io>
2024-11-08 14:37:34 +0100

    ajout de game-loop dans tictactoe

--- tictactoe.py
+++ tictactoe.py
@@ -113,3 +113,21 @@ while True: #Boucle qui fait tourner le jeu jusqu'à la victoire ou match nul
         plateau_jeu()
         print('Belle partie,\nMatch nul')
         break #Match nul, Fin de partie, fin du programme
+while True:
+    Game_mode = str(input("Quel mode souahitez vous jouer ?\nVous avez le choix entre ces deux modes : \n \
+                          Tapez PVP pour jouer contre un autre joueur \n \
+                          Tapez PVE pour jouer contre une intelligence artificielle \nTapez Q pour quitter. \n Quel choix faites vous? : "))
+    
+    if Game_mode == "PVP":
+        # Appeler la fonction pour le mode PVP
+        mode_PVP()
+        break
+    elif Game_mode == "PVE":
+        # Appeler la fonction pour le mode PVE
+        mode_PVE()
+        break
+    elif Game_mode == "Q":
+        print("Merci d'avoir joué ! À bientôt.")
+        break
+    else:
+        print("Choix invalide. Veuillez réessayer.")
